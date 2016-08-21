3f0ea9a50d98befd3d08d8bbbba9bc612422fbde
Tyler Powers <gr4yscale@gmail.com>
2016-08-21 21:23:25 +0100

    remove dead code from another project

--- selfie/src/redux/reducers/appReducer.js
+++ selfie/src/redux/reducers/appReducer.js
@@ -6,28 +6,6 @@ const initialState = {
 
 export default function app(state = initialState, action) {
   switch (action.type) {
-    // case actionTypes.LOGIN_SUCCESS:
-    //   return Object.assign({}, state, {
-    //     ...state,
-    //     ...{currentUser: action.response},
-    //   })
-    // case actionTypes.LINKS_RECEIVED_SUCCESS:
-    //   let friends = state.friends
-    //   let links = action.response.results.map(
-    //     (link) => {
-    //       let sender = _.find(friends, {objectId: link.sender_id})
-    //       let senderDisplayName = _.get(sender, 'displayName') ? sender.displayName : 'Unknown Sender'
-    //       // mutating state here, but I don't care...it seems expensive to alloc new objects for this
-    //       return {
-    //         ...link,
-    //         senderDisplayName,
-    //       }
-    //     }
-    //   )
-    //   return Object.assign({}, state, {
-    //     links,
-    //   })
-
     case actionTypes.DEVICE_IDENTIFIER_DISCOVERED:
       return Object.assign({}, state, {
         deviceIdentifier: action.payload,
